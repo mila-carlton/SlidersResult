be727bf6538c76b744126d934bd7ffc00750a118
mila-carlton <mila.carlton@mail.ru>
2023-10-01 00:13:17 +0400

    Initial Commit

---
+++ Sliders/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sliders
+//
+//  Created by PASGON TEXTILE on 01.10.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
