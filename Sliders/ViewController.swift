30e1d861f5e0412d4746cdd0f37ba43b97fbc236
mila-carlton <mila.carlton@mail.ru>
2023-10-01 01:03:06 +0400

    todaySlider

--- Sliders/ViewController.swift
+++ Sliders/ViewController.swift
@@ -1,9 +1,4 @@
-//
-//  ViewController.swift
-//  Sliders
-//
-//  Created by PASGON TEXTILE on 01.10.23.
-//
+
 
 import UIKit
 
@@ -11,7 +6,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
